6417796d0ab87544d3d31c4aa1fb5e06f2496eeb
Benedikt Grether <info@benediktgrether.de>
2025-05-20 15:51:40 +0200

    Add tailwind

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,24 @@
 import { LitElement, html } from 'lit';
+import tailwindCss from '../../index.css' assert { type: 'css' };
 
 export class Button extends LitElement {
     static properties = { label: { type: String }, href: { type: String } };
 
+    static styles = [tailwindCss];
+
     render() {
         if (this.href) {
-            return html`<a href="${this.href}"><slot /></a>`;
+            return html`<a
+                class="bg-blue-300 hover:blue-500 text-white font-bold py-2 px-4 rounded"
+                href="${this.href}"
+                ><slot
+            /></a>`;
         }
-        return html`<button><slot /></button>`;
+        return html`<button
+            class="bg-blue-300 hover:blue-500 text-white font-bold py-2 px-4 rounded"
+        >
+            <slot />
+        </button>`;
     }
 }
 customElements.define('ds-button', Button);
